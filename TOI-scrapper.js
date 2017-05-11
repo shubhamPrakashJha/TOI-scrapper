bd26608e4f9f6649b60ff831a7e1d35cd6036e30
Shubham Prakash <sjonline4u@gmail.com>
2017-05-11 11:53:00 +0530

    Initial commit

---
+++ TOI-scrapper.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Shubham Prakash on 5/11/2017.
+ */
+
+var arraysOfHeading = document.getElementsByTagName("li");
+for(i=0;i<arraysOfHeading.length;i++) {
+    console.log(arraysOfHeading[i].innerText);
+};
